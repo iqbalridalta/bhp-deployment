57acf9ebe42e5c9ea13a54465cbacf6520b0dddc
Iqbal Ridalta Putra <55306876+iqbalridalta@users.noreply.github.com>
2020-12-27 23:30:21 +0700

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from import util
+import .util
 # import util
 
 app = Flask(__name__, static_url_path="/client", static_folder='../client', template_folder="../client")
